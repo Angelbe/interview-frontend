11b09164d1a09359215c975c17522c462bc1fccd
Angel <bernard.angel93@gmail.com>
2022-09-13 14:17:30 +0200

    solved 6

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Card from "./components/Card";
 //3. create a Card component with name, gender and an image of the character, pass it one character and put it in the middle of the screen.
 //4. Create a row of 4 cards
 //5. Now create a grid/table of 3 rows and 4 columns with 12 characters
+//6. create a button which sorts names by A→Z or Z→A when pressed
 
 const getCharacters = () => {
     return axios
@@ -19,17 +20,39 @@ const getCharacters = () => {
 
 export default function App() {
     const [characters, setCharacters] = useState([]);
+    const [charactersModified, setCharactersModified] = useState([]);
+    const [sortType, setSortType] = useState("alphabetically");
 
     useEffect(() => {
         getCharacters().then((response) => {
-            setCharacters(response.results);
+            setCharacters(response.results.slice(0, 12));
+            setCharactersModified(response.results.slice(0, 12));
         });
     }, []);
 
+    useEffect(() => {
+        if (sortType === "alphabetically") {
+            setCharactersModified(characters.slice(0, 12).sort());
+        }
+        if (sortType === "unalphabetically") {
+            setCharactersModified(characters.slice(0, 12).reverse());
+        }
+    }, [sortType]);
+
+    const handleSortClick = () => {
+        if (sortType === "alphabetically") {
+            setSortType("unalphabetically");
+        }
+        if (sortType === "unalphabetically") {
+            setSortType("alphabetically");
+        }
+    };
+
     return (
         <div className="App">
+            <button onClick={handleSortClick}> sort Characters</button>
             <div className="CardContainer">
-                {characters.slice(0, 12).map((character) => (
+                {charactersModified.map((character) => (
                     <Card key={character.name} character={character} />
                 ))}
             </div>
