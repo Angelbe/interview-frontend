7cbdaee75ebdcf7d21edc1c83e0d91d5d3d29463
Angel <bernard.angel93@gmail.com>
2022-09-13 14:07:27 +0200

    solved 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./styles.css";
 import axios from "axios";
+import { useEffect, useState } from "react";
 //1. Call this API (https://rickandmortyapi.com/api/character) when you click this button
+//2. Fetch all data on first render and save it on state.
 
 const getCharacters = () => {
     return axios
@@ -12,6 +14,16 @@ const getCharacters = () => {
 };
 
 export default function App() {
+    const [characters, setCharacters] = useState([]);
+
+    useEffect(() => {
+        getCharacters().then((response) => {
+            setCharacters(response.results);
+        });
+    }, []);
+
+    console.log(characters);
+
     return (
         <div className="App">
             <button onClick={getCharacters}>cal API</button>
