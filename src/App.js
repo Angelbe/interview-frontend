dfa814be3bb4dc740a2123daee7f4582b6494ce8
Angel <bernard.angel93@gmail.com>
2022-09-13 13:31:49 +0200

    solved 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import "./styles.css";
-
+import axios from "axios";
 //1. Call this API (https://rickandmortyapi.com/api/character) when you click this button
 
+const getCharacters = () => {
+    return axios
+        .get("https://rickandmortyapi.com/api/character")
+        .then((response) => {
+            return response.data;
+        })
+        .catch((error) => console.log(error));
+};
+
 export default function App() {
-  return (
-      <div className="App">
-        <button onClick={getCharacters}>cal API</button>
-      </div>
-  );
+    return (
+        <div className="App">
+            <button onClick={getCharacters}>cal API</button>
+        </div>
+    );
 }
